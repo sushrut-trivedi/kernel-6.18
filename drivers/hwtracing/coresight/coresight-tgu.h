f4ff7f142ecfc35970f6aec743ff0b88ea873bf4
Jie Gan <jie.gan@oss.qualcomm.com>
2026-05-12 09:02:23 +0800

    Revert "FROMLIST: coresight-tgu: add support to configure next action"
    This reverts commit 78172ff966101276aa3805305ff692e41cf2aa62. This is
    an old version of the TGU patch. An upstream version already exists.
    Revise to the latest version, V14, of the upstream patch.
    
    Signed-off-by: Jie Gan <jie.gan@oss.qualcomm.com>

--- drivers/hwtracing/coresight/coresight-tgu.h
+++ drivers/hwtracing/coresight/coresight-tgu.h
@@ -50,7 +50,6 @@
 #define STEP_OFFSET 0x1D8
 #define PRIORITY_START_OFFSET 0x0074
 #define CONDITION_DECODE_OFFSET 0x0050
-#define CONDITION_SELECT_OFFSET 0x0060
 #define PRIORITY_OFFSET 0x60
 #define REG_OFFSET 0x4
 
@@ -61,9 +60,6 @@
 
 #define CONDITION_DECODE_STEP(step, decode) \
 	(CONDITION_DECODE_OFFSET + REG_OFFSET * decode + STEP_OFFSET * step)
-
-#define CONDITION_SELECT_STEP(step, select) \
-	(CONDITION_SELECT_OFFSET + REG_OFFSET * select + STEP_OFFSET * step)
 
 #define tgu_dataset_rw(name, step_index, type, reg_num)                  \
 	(&((struct tgu_attribute[]){ {                                   \
@@ -79,9 +75,6 @@
 
 #define STEP_DECODE(step_index, reg_num) \
 	tgu_dataset_rw(reg##reg_num, step_index, TGU_CONDITION_DECODE, reg_num)
-
-#define STEP_SELECT(step_index, reg_num) \
-	tgu_dataset_rw(reg##reg_num, step_index, TGU_CONDITION_SELECT, reg_num)
 
 #define STEP_PRIORITY_LIST(step_index, priority)  \
 	{STEP_PRIORITY(step_index, 0, priority),  \
@@ -113,15 +106,6 @@
 	 NULL           \
 	}
 
-#define STEP_SELECT_LIST(n) \
-	{STEP_SELECT(n, 0), \
-	 STEP_SELECT(n, 1), \
-	 STEP_SELECT(n, 2), \
-	 STEP_SELECT(n, 3), \
-	 STEP_SELECT(n, 4), \
-	 NULL           \
-	}
-
 #define PRIORITY_ATTRIBUTE_GROUP_INIT(step, priority)\
 	(&(const struct attribute_group){\
 		.attrs = (struct attribute*[])STEP_PRIORITY_LIST(step, priority),\
@@ -136,20 +120,12 @@
 		.name = "step" #step "_condition_decode" \
 	})
 
-#define CONDITION_SELECT_ATTRIBUTE_GROUP_INIT(step)\
-	(&(const struct attribute_group){\
-		.attrs = (struct attribute*[])STEP_SELECT_LIST(step),\
-		.is_visible = tgu_node_visible,\
-		.name = "step" #step "_condition_select" \
-	})
-
 enum operation_index {
 	TGU_PRIORITY0,
 	TGU_PRIORITY1,
 	TGU_PRIORITY2,
 	TGU_PRIORITY3,
 	TGU_CONDITION_DECODE,
-	TGU_CONDITION_SELECT,
 };
 
 /* Maximum priority that TGU supports */
@@ -165,7 +141,6 @@ struct tgu_attribute {
 struct value_table {
 	unsigned int *priority;
 	unsigned int *condition_decode;
-	unsigned int *condition_select;
 };
 
 /**
@@ -179,7 +154,6 @@ struct value_table {
  * @max_reg: Maximum number of registers
  * @max_step: Maximum step size
  * @max_condition_decode: Maximum number of condition_decode
- * @max_condition_select: Maximum number of condition_select
  *
  * This structure defines the data associated with a TGU device,
  * including its base address, device pointers, clock, spinlock for
@@ -196,7 +170,6 @@ struct tgu_drvdata {
 	int max_reg;
 	int max_step;
 	int max_condition_decode;
-	int max_condition_select;
 };
 
 #endif
